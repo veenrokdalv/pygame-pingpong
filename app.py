00614cd1bb529a38853211868f61778d360742ed
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-11-18 21:35:58 +0300

    В калассе App() описан метод: draw() - Отрисовывает все объекты программы.
    В методе run() Добавлено:
    Контроль производительности программы,
    draw()

--- app.py
+++ app.py
@@ -6,47 +6,55 @@ import ball
 
 
 class App:
-  """
-  Класс приложения.
-  Управляет всеми процессами приложения.
-  """
-  def __init__(self):
-    """Инициализация главных настроек приложения"""
+    """
+    Класс приложения.
+    Управляет всеми процессами приложения.
+    """
 
-    # Настройки окна.
-    self.WIDTH = 600
-    self.HEIGHT = 900
-    self.TITLE = "Ping Pong"
-    self.ICON = pg.image.load('images/icon_app.png')
-    self.WINDOW = pg.display.set_mode((self.WIDTH, self.HEIGHT))
-    pg.display.set_caption(self.TITLE)
-    pg.display.set_icon(self.ICON)
+    def __init__(self):
+        """Инициализация главных настроек приложения"""
 
-    # Настройки производительности
-    self._CLOCK = pg.time.Clock()
-    self._fps = 30
+        # Настройки окна.
+        self.WIDTH = 600
+        self.HEIGHT = 900
+        self.TITLE = "Ping Pong"
+        self.ICON = pg.image.load('images/icon_app.png')
+        self.WINDOW = pg.display.set_mode((self.WIDTH, self.HEIGHT))
+        pg.display.set_caption(self.TITLE)
+        pg.display.set_icon(self.ICON)
 
-    # Инициализация платформ
-    self._PLATFORM_TOP = platform.Platform()
-    self._PLATFORM_BOTTOM = platform.Platform()
+        # Настройки производительности
+        self._CLOCK = pg.time.Clock()
+        self._fps = 30
 
-    # Инициализация шарика
-    self._BALL = ball.Ball()
-  
-  def run(self):
-    """Запуск основного цикла программы"""
-    while True:
-      self.check_events()
-  
-  def quit(self):
-    """Завершение программы"""
-    print('Правильное завершение программы')
-    pg.quit()
-    quit()
-    sys.exit()
+        # Инициализация платформ
+        self._PLATFORM_TOP = platform.Platform()
+        self._PLATFORM_BOTTOM = platform.Platform()
 
-  def check_events(self):
-    """Отлавливание всех событий программы"""
-    
-    # Выход из программы путем нажатия крестика
-    [self.quit() for event in pg.event.get() if event.type == pg.QUIT]
+        # Инициализация шарика
+        self._BALL = ball.Ball()
+
+    def run(self):
+        """Запуск основного цикла программы"""
+        while True:
+            self.check_events()
+            self.draw()
+            self._CLOCK(self._fps)
+
+    def quit(self):
+        """Завершение программы"""
+        print('Правильное завершение программы')
+        pg.quit()
+        quit()
+        sys.exit()
+
+    def check_events(self):
+        """Отлавливание всех событий программы"""
+
+        # Выход из программы путем нажатия крестика
+        [self.quit() for event in pg.event.get() if event.type == pg.QUIT]
+
+    def draw(self):
+      """Отрисовывает и отображает все объекты программы"""
+
+      pg.display.update()
