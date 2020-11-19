dfbf68bd991a610f1b6d9a8b95bc4bdff64f13c4
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-11-19 13:12:50 +0300

    В классе Platform: Добавлен метод draw()

--- platform.py
+++ platform.py
@@ -1,20 +1,35 @@
 import pygame as pg
+
+
 class Platform:
-  """
-  Класс платформы.
-  Описан весь функционал платформы платформы.
-  """
-  def __init__(self, window, start_pos_y):
-    """Инициализация главных настроек платформы"""
-    
-    # Параметры поверхности
-    self.WIDTH = 130
-    self.HEIGHT = 40
-    self.pos_x = pg.display.get_window_size()[0] // 2 - self.WIDTH // 2
-    self.pos_y = start_pos_y + 50
-    self.color = (102, 51, 0)
-    
-    # Колличество очков
-    self.score = 0
+    """
+    Класс платформы.
+    Описан весь функционал платформы платформы.
+    """
+
+    def __init__(self, WINDOW, start_pos_y):
+        """Инициализация главных настроек платформы"""
+
+        # Ссылка на окно
+        self.WINDOW = WINDOW
+
+        # Параметры поверхности
+        self.WIDTH = 130
+        self.HEIGHT = 20
+        self.pos_x = pg.display.get_window_size()[0] // 2 - self.WIDTH // 2
+        self.pos_y = start_pos_y + 50 - self.HEIGHT
+        self.color = (102, 51, 0)
+        self.rect = (self.pos_x, self.pos_y, self.WIDTH, self.HEIGHT)
+
+        # Колличество очков
+        self.score = 0
 
 
+    def draw(self):
+        """
+        Отрисовывает платформу
+        в координатах self.pos_x, self.pos_y,
+        с цветом self.color,
+        и размерами self.WIDTH, self.HEIGH
+        """
+        pg.draw.rect(self.WINDOW, self.color, self.rect)
