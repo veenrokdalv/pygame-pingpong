2ccf856b5cda496937448e42b70a2e39f4bb55d5
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-11-19 12:59:30 +0300

    В файле platform.py: добавлен иморт pygame.
    В классе Platform добавлен метод инициализации параметров поверхности

--- platform.py
+++ platform.py
@@ -1,6 +1,20 @@
+import pygame as pg
 class Platform:
   """
   Класс платформы.
   Описан весь функционал платформы платформы.
   """
-  pass
+  def __init__(self, window, start_pos_y):
+    """Инициализация главных настроек платформы"""
+    
+    # Параметры поверхности
+    self.WIDTH = 130
+    self.HEIGHT = 40
+    self.pos_x = pg.display.get_window_size()[0] // 2 - self.WIDTH // 2
+    self.pos_y = start_pos_y + 50
+    self.color = (102, 51, 0)
+    
+    # Колличество очков
+    self.score = 0
+
+
