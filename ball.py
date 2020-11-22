f1ac14a192cdf4ffed219c9d80ad29cb1733f89d
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-11-22 16:40:12 +0300

    В классе Ball: Были изменены параметры скорости шарика

--- ball.py
+++ ball.py
@@ -1,6 +1,7 @@
 import random
 import pygame as pg
 
+
 class Ball:
     """
     Класс шарика.
@@ -22,14 +23,12 @@ class Ball:
         self.pos_y = pg.display.get_window_size()[1] // 2
 
         # Начально направление
-        self.speed_x = random.choice([-6, -8, -10, 6, 8, 10])
-        self.speed_y = random.choice([-6, -8, -10, 6, 8, 10])
+        self.speed_x = random.choice([-4, -5, -6, 4, 5, 6])
+        self.speed_y = random.choice([-4, -5, -6, 4, 5, 6])
 
         # Характеристики
         self.RADIUS = 20
         self.color = (255, 0, 51)
-
-
 
     def draw(self):
         """Отрисовывает мячик в координатах self.pos_x, self.pos_y, цветом self.color, радиусом self.radius"""
@@ -47,14 +46,15 @@ class Ball:
         if self.pos_x - self.RADIUS < 0 or self.pos_x + self.RADIUS + self.speed_x > pg.display.get_window_size()[0]:
             self.speed_x = -self.speed_x
         elif self.pos_y - self.RADIUS < 0 or self.pos_y + self.RADIUS + self.speed_y > pg.display.get_window_size()[1]:
-                self.set_start_position()
-                self.set_random_speed()
+            self.set_start_position()
+            self.set_random_speed()
 
     def set_start_position(self):
         """Возвращает мячик в центр экрана"""
         self.pos_x = pg.display.get_window_size()[0] // 2
         self.pos_y = pg.display.get_window_size()[1] // 2
+
     def set_random_speed(self):
         """Устанавливает новые параметры скоростей для обеих осей"""
-        self.speed_x = random.choice([-6, -8, -10, 6, 8, 10])
-        self.speed_y = random.choice([-6, -8, -10, 6, 8, 10])
+        self.speed_x = random.choice([-4, -5, -6, 4, 5, 6])
+        self.speed_y = random.choice([-4, -5, -6, 4, 5, 6])
