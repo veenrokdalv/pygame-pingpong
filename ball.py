4f289e8c2c701df6888ba4f2c3bdca173f53ccda
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-11-22 16:35:21 +0300

    В классе Ball(): Добавлены методы: set_start_position() set_random_speed() В методе collision_wall: При регистрации касания шарика с нижней или верхней границ экрана шарик - возвращается на стартовую позицию

--- ball.py
+++ ball.py
@@ -1,7 +1,6 @@
 import random
 import pygame as pg
 
-
 class Ball:
     """
     Класс шарика.
@@ -48,4 +47,14 @@ class Ball:
         if self.pos_x - self.RADIUS < 0 or self.pos_x + self.RADIUS + self.speed_x > pg.display.get_window_size()[0]:
             self.speed_x = -self.speed_x
         elif self.pos_y - self.RADIUS < 0 or self.pos_y + self.RADIUS + self.speed_y > pg.display.get_window_size()[1]:
-            self.speed_y = -self.speed_y
+                self.set_start_position()
+                self.set_random_speed()
+
+    def set_start_position(self):
+        """Возвращает мячик в центр экрана"""
+        self.pos_x = pg.display.get_window_size()[0] // 2
+        self.pos_y = pg.display.get_window_size()[1] // 2
+    def set_random_speed(self):
+        """Устанавливает новые параметры скоростей для обеих осей"""
+        self.speed_x = random.choice([-6, -8, -10, 6, 8, 10])
+        self.speed_y = random.choice([-6, -8, -10, 6, 8, 10])
