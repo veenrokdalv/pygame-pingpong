5efa01c1459e312e2fd41e05fa03f06448affeb9
vlad_korneev_2329 <vladkorneev2329@gmail.com>
2020-11-23 09:27:33 +0300

    В классе Ball добавлен метод collision_platform.

--- ball.py
+++ ball.py
@@ -38,6 +38,7 @@ class Ball:
     def move(self):
         """Изменяет координаты self.pos_x, self.pos_y"""
         self.collision_wall()
+        self.collision_platform()
         self.pos_x += self.speed_x
         self.pos_y += self.speed_y
 
@@ -48,6 +49,25 @@ class Ball:
         elif self.pos_y - self.RADIUS < 0 or self.pos_y + self.RADIUS + self.speed_y > pg.display.get_window_size()[1]:
             self.set_start_position()
             self.set_random_speed()
+   
+   
+    def collision_platform(self):
+        """Проверка на столкновение c платформой"""
+        # Нижняя сторона шарика с верхней стороной нижней доски
+        if self.pos_y + self.RADIUS > self.PLATFORM_BOTTOM.pos_y:
+            # Нижняя леваая сторона шарика с верхней правой стороной нижней доски
+            if self.pos_x - self.RADIUS < self.PLATFORM_BOTTOM.pos_x + self.PLATFORM_BOTTOM.WIDTH:
+                # Нижняя правая сторона шарика с верхней левой стороной нижней доски
+                if self.pos_x + self.RADIUS > self.PLATFORM_BOTTOM.pos_x:
+                    self.speed_y = - self.speed_y   
+        
+        # Верхняя сторона шарика с нижней стороной верхней доски
+        if self.pos_y - self.RADIUS < self.PLATFORM_TOP.pos_y + self.PLATFORM_TOP.HEIGHT:
+            # Верхняя леваая сторона шарика с нижней левой стороной верхней доски
+            if self.pos_x - self.RADIUS < self.PLATFORM_TOP.pos_x + self.PLATFORM_TOP.WIDTH:
+                # Верхняя правая сторона шарика с нижней правой стороной верхней доски
+                if self.pos_x + self.RADIUS > self.PLATFORM_TOP.pos_x:
+                    self.speed_y = - self.speed_y 
 
     def set_start_position(self):
         """Возвращает мячик в центр экрана"""
