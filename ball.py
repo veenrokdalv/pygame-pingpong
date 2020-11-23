77423f664cb078bd7e21d4fdfe4cfc47ac8ed742
Vlad <veenrokdalv@protonmail.com>
2020-11-23 16:54:59 +0300

    В классе Ball добавлены методы restart и goal: Так же были внесены изменения в move(), collision_wall(). Теперь collision_wall регистрирует попадание мячика с левой и правой границей. В move() Были введены проверки, для изменения вектора мячика.

--- ball.py
+++ ball.py
@@ -37,44 +37,67 @@ class Ball:
 
     def move(self):
         """Изменяет координаты self.pos_x, self.pos_y"""
-        self.collision_wall()
-        self.collision_platform()
+        if self.collision_wall:
+            self.speed_x = -self.speed_x
+        if self.collision_platform:
+            self.speed_y = -self.speed_y
         self.pos_x += self.speed_x
         self.pos_y += self.speed_y
 
+    @property
     def collision_wall(self):
         """Проверяет на столкновение с границами экрана"""
-        if self.pos_x - self.RADIUS < 0 or self.pos_x + self.RADIUS + self.speed_x > pg.display.get_window_size()[0]:
-            self.speed_x = -self.speed_x
-        elif self.pos_y - self.RADIUS < 0 or self.pos_y + self.RADIUS + self.speed_y > pg.display.get_window_size()[1]:
-            self.set_start_position()
-            self.set_random_speed()
+        return self.pos_x - self.RADIUS < 0 or self.pos_x + self.RADIUS + self.speed_x > pg.display.get_window_size()[0]
+        
    
-   
+    @property
     def collision_platform(self):
         """Проверка на столкновение c платформой"""
         # Нижняя сторона шарика с верхней стороной нижней доски
-        if self.pos_y + self.RADIUS > self.PLATFORM_BOTTOM.pos_y:
+        if self.pos_y + self.RADIUS == self.PLATFORM_BOTTOM.pos_y:
             # Нижняя леваая сторона шарика с верхней правой стороной нижней доски
             if self.pos_x - self.RADIUS < self.PLATFORM_BOTTOM.pos_x + self.PLATFORM_BOTTOM.WIDTH:
                 # Нижняя правая сторона шарика с верхней левой стороной нижней доски
                 if self.pos_x + self.RADIUS > self.PLATFORM_BOTTOM.pos_x:
-                    self.speed_y = - self.speed_y   
+                    return True   
         
         # Верхняя сторона шарика с нижней стороной верхней доски
-        if self.pos_y - self.RADIUS < self.PLATFORM_TOP.pos_y + self.PLATFORM_TOP.HEIGHT:
+        if self.pos_y - self.RADIUS == self.PLATFORM_TOP.pos_y + self.PLATFORM_TOP.HEIGHT:
             # Верхняя леваая сторона шарика с нижней левой стороной верхней доски
             if self.pos_x - self.RADIUS < self.PLATFORM_TOP.pos_x + self.PLATFORM_TOP.WIDTH:
                 # Верхняя правая сторона шарика с нижней правой стороной верхней доски
                 if self.pos_x + self.RADIUS > self.PLATFORM_TOP.pos_x:
-                    self.speed_y = - self.speed_y 
+                    return True
+        return False
+
+
+    def goal(self):
+        """Проверка, какую границу пересек мячик
+
+        Returns:
+            ['top']: Если пересек верхнюю границу
+            ['bottom']: Если пересек нижнюю границу
+
+        """
+        if self.pos_y - self.RADIUS < 0:
+            return 'top'
+        elif self.pos_y + self.RADIUS + self.speed_y > pg.display.get_window_size()[1]:
+            return 'bottom'
 
     def set_start_position(self):
         """Возвращает мячик в центр экрана"""
         self.pos_x = pg.display.get_window_size()[0] // 2
         self.pos_y = pg.display.get_window_size()[1] // 2
 
+
     def set_random_speed(self):
         """Устанавливает новые параметры скоростей для обеих осей"""
         self.speed_x = random.choice([-4, -5, -6, 4, 5, 6])
         self.speed_y = random.choice([-4, -5, -6, 4, 5, 6])
+
+
+    def restart(self):
+        """Устанавливает начальное положение мячика и рандомную скорость
+        """
+        self.set_random_speed()
+        self.set_start_position()
